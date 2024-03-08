c5308213611dfd692b788d524c07862fb80a5501
MarinoFajardo <marinofajardo@correo.ugr.es>
2024-03-08 19:25:13 +0100

    Configuración de Express y ESModule para el servidor.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hola Mundo");
+/**
+ * Cargar las dependencias
+ */
+import express from 'express' //Express
+
+/**
+ * Creación de la Aplicación.
+ */
+const app = express();
+
+/**
+ * Routing
+ */
+app.get('/',function(req,res){
+    res.send('Hola Mundo en Express');
+});
+
+/**
+ * Definición del puerto y arranque de la app
+ */
+const port = 3000;
+app.listen(port, () => {
+    console.log('El servidor está funcionando')
+});
